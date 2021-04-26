dd01d4cb4b453aae4084a7b1b021b09d5ff59c92
weichen <weichen@dentall.io>
2021-04-26 09:04:23 +0800

    Add title padding

--- assets/js/ht-card.js
+++ assets/js/ht-card.js
@@ -32,3 +32,13 @@ cardList.forEach(id => {
     document.getElementById(id).addEventListener('mouseenter', (e) => mouseEnterCardHandler(e));
     document.getElementById(id).addEventListener('mouseleave', (e) => mouseLeaveCardHandler(e));
 });
+
+function scrollToTop() {
+    const scrollElement = window.document.scrollingElement || window.document.body || window.document.documentElement;
+    anime({
+        targets: scrollElement,
+        scrollTop: 0,
+        duration: 500,
+        easing: 'easeInOutQuad'
+    });
+}
