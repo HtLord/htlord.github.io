e9a8c876598dcf2b4fc23ffaddb752199c82a75f
htlord <s75757775@hotmail.com>
2024-01-11 20:37:25 +0800

    Add toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
-import { defineConfig } from 'astro/config';
+import remarkToc from 'remark-toc';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://htlord.github.io',
-  integrations: [
-    mdx(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
+    markdown: {
+        // Applied to .md and .mdx files
+        remarkPlugins: [remarkToc],
+        rehypePlugins: [rehypeAccessibleEmojis],
+    },
+    site: 'https://htlord.github.io',
+    integrations: [
+        mdx(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        sitemap(),
+    ],
 });
