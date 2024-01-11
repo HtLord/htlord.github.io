7d3bbd2a5049ab9bd23b170abd0ab6d2dd43ed0b
htlord <s75757775@hotmail.com>
2024-01-11 22:51:26 +0800

    Refactor SITE values

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'HtLord 學習日誌';
-export const SITE_DESCRIPTION = '';
-export const SITE_PAGES = 5;
-
 export const SITE = {
     title: 'HtLord 學習日誌',
     author: 'htlord',
