3d06a82562804c5e81fbc1bd8de3e2a2e5805e09
htlord <s75757775@hotmail.com>
2024-01-14 13:43:53 +0800

    Impl experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import yaml from '@rollup/plugin-yaml';
 
-// https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [
+            yaml()
+        ]
+    },
     markdown: {
-        remarkPlugins: [ [remarkToc, { heading: "ToC"} ] ],
-        rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
+        remarkPlugins: [[remarkToc, {heading: "ToC"}]],
+        rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {behavior: 'append'}]],
     },
     site: 'https://htlord.github.io',
     integrations: [
